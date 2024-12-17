6e9902181b9ca24026b8e2cef23988cfba35810a
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-12-17 14:03:50 -0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Jibryll Brinkley on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
